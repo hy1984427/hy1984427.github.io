223adc09ddb5612b0d8b11492ef842ce826de217
hy1984427 <hy1984427@gmail.com>
2015-07-20 14:23:13 +0800

    Add two articles.

---
+++ documents/PageName_FeatureName_StoryID.mm
@@ -0,0 +1,125 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1332917033625" ID="Freemind_Link_924050207" MODIFIED="1335174071315" TEXT="Feature_name">
+<node CREATED="1332917565057" ID="Freemind_Link_1737636160" MODIFIED="1335175621281" POSITION="right" TEXT="Story_ID: Description">
+<node CREATED="1332917191895" ID="_" MODIFIED="1335175639064" TEXT="A field labeled &quot;xxxx&quot;">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1332917723336" ID="Freemind_Link_987476006" MODIFIED="1335176555101" TEXT="A drop down list under label question">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1332917753226" ID="Freemind_Link_105793997" MODIFIED="1333270078340" TEXT="Default value is &quot;Select&quot;">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1332917935913" ID="Freemind_Link_1488459267" MODIFIED="1333270087106" TEXT="User can see values in drop down menu">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1332917970288" ID="Freemind_Link_1772564993" MODIFIED="1335176151676" TEXT="&quot;A&quot;"/>
+<node CREATED="1332917988764" ID="Freemind_Link_1708993876" MODIFIED="1335176156760" TEXT="&quot;B&quot;"/>
+<node CREATED="1332917994880" ID="Freemind_Link_381385953" MODIFIED="1335176162670" TEXT="&quot;C&quot;"/>
+</node>
+<node CREATED="1332918882393" ID="Freemind_Link_847274006" MODIFIED="1333270282529" TEXT="User doesn&apos;t select any value and click &quot;Continue&quot;">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1332918912556" ID="Freemind_Link_1856195350" MODIFIED="1335175780758" TEXT="User can not proceed to next page and get the error message &quot;xxxx&quot;">
+<node CREATED="1333251290208" ID="Freemind_Link_1171011115" MODIFIED="1333251300633" TEXT="shouldRejectIfConstructionPeriodIsEmpty">
+<icon BUILTIN="penguin"/>
+</node>
+</node>
+</node>
+<node CREATED="1332917779827" ID="Freemind_Link_471116803" MODIFIED="1335176316568" TEXT="User select &quot;A&quot;">
+<node CREATED="1332918504210" ID="Freemind_Link_1041145483" MODIFIED="1335175815825" TEXT="User can see another question: &quot;xxxx&quot; at the right side">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1334560831983" ID="Freemind_Link_1772675681" MODIFIED="1334560882601" TEXT="home-details-spec.js">
+<icon BUILTIN="penguin"/>
+<node CREATED="1334560834823" ID="Freemind_Link_1934272204" MODIFIED="1334560844261" TEXT="should show year of construction when LAST_TEN_YEARS is selected"/>
+<node CREATED="1334563086639" ID="Freemind_Link_376614263" MODIFIED="1334563088859" TEXT="should show year of construction if LAST_TEN_YEARS is selected on page load"/>
+</node>
+</node>
+<node CREATED="1332918292107" ID="Freemind_Link_610375111" MODIFIED="1335176340553" TEXT="User can see another drop down menu underneath the question">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1332918535429" ID="Freemind_Link_911599109" MODIFIED="1333270489499" TEXT="Default value is &quot;Select&quot;">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1332918703272" ID="Freemind_Link_483510691" MODIFIED="1333270494593" TEXT="User can see values in drop down menu">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1332918714997" ID="Freemind_Link_227810777" MODIFIED="1335176207955" TEXT="&quot;1&quot;"/>
+<node CREATED="1332918724926" ID="Freemind_Link_1972865832" MODIFIED="1335176212387" TEXT="&quot;2&quot;"/>
+<node CREATED="1332918729199" ID="Freemind_Link_1504539165" MODIFIED="1335176216715" TEXT="&quot;3&quot;"/>
+</node>
+</node>
+<node CREATED="1332919017961" ID="Freemind_Link_1494992554" MODIFIED="1335176655552" TEXT="User doesn&apos;t select any value in second drop down and click &quot;Continue&quot;">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1332918912556" ID="Freemind_Link_1423671791" MODIFIED="1335176377251" TEXT="User can not proceed to next page and get the error message &quot;xxxx&quot;">
+<node CREATED="1333251323044" ID="Freemind_Link_813816031" MODIFIED="1333251346693" TEXT="shouldRejectIfConstructionYearIsEmptyGivenLastTenYearsPeriodSelected">
+<icon BUILTIN="penguin"/>
+</node>
+</node>
+</node>
+<node CREATED="1332919215191" ID="Freemind_Link_181684366" MODIFIED="1335176395555" TEXT="User select one of values in the drop down e.g. &quot;1&quot;">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1332922432204" ID="Freemind_Link_892224551" MODIFIED="1333270704453" TEXT="User click &quot;continue&quot;">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1332922448117" ID="Freemind_Link_1100981054" MODIFIED="1332922492430" TEXT="User can proceed to the next page">
+<node CREATED="1333251623593" ID="Freemind_Link_1605521776" MODIFIED="1333251639796" TEXT="shouldNotRejectGivenLastTenYearsPeriodAndSpecificYearSelected">
+<icon BUILTIN="penguin"/>
+</node>
+</node>
+</node>
+<node CREATED="1332922498716" ID="Freemind_Link_1961205188" MODIFIED="1335176429980" TEXT="User select another value in first drop down e.g. &quot;B&quot;">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1332922569770" ID="Freemind_Link_1626667863" MODIFIED="1335176449747" TEXT="second drop down disappear">
+<node CREATED="1332922669963" ID="Freemind_Link_202567786" MODIFIED="1335176460995" TEXT="User select &quot;A&quot; again">
+<node CREATED="1332922702236" ID="Freemind_Link_737977871" MODIFIED="1335176480378" TEXT="second drop down appear"/>
+<node CREATED="1332922728321" ID="Freemind_Link_654891346" MODIFIED="1335176501016" TEXT="User can see the value he selected previously e.g. &quot;1&quot; in second drop down">
+<node CREATED="1334560831983" ID="Freemind_Link_1186913635" MODIFIED="1334560882601" TEXT="home-details-spec.js">
+<icon BUILTIN="penguin"/>
+<node CREATED="1334560834823" ID="Freemind_Link_751150288" MODIFIED="1334562898438" TEXT="should remember the value of year of construction"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1332922837215" ID="Freemind_Link_1246207594" MODIFIED="1335176524671" TEXT="User select any of values except &quot;A&quot; and click &quot;Continue&quot;">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1332922870066" ID="Freemind_Link_1163431380" MODIFIED="1332923045846" TEXT="User can proceed to the next page">
+<node CREATED="1333251449620" ID="Freemind_Link_183549585" MODIFIED="1333251473879" TEXT="shouldNotRejectGivenConstructionPeriodOtherThanLastTenYearsSelected">
+<icon BUILTIN="penguin"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1332232029915" ID="Freemind_Link_1517456868" MODIFIED="1332232106456" POSITION="left" TEXT="X Browsers Testing">
+<node CREATED="1332232108401" ID="Freemind_Link_1389306967" MODIFIED="1332232117948" TEXT="IE">
+<node CREATED="1332232119064" ID="Freemind_Link_487421998" MODIFIED="1333603539422" TEXT="7">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1332232123071" ID="Freemind_Link_528107922" MODIFIED="1333605416283" TEXT="8">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1332232124281" ID="Freemind_Link_1009194989" MODIFIED="1333270772220" TEXT="9">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1332232128787" ID="Freemind_Link_1563867194" MODIFIED="1332232133334" TEXT="Firefox">
+<node CREATED="1332232584288" ID="Freemind_Link_1503242921" MODIFIED="1333272314040" TEXT="3.6.25">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1332232588639" ID="Freemind_Link_1604915596" MODIFIED="1333271954295" TEXT="Latest (11)">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1332232134888" ID="Freemind_Link_860741134" MODIFIED="1332232160185" TEXT="Chrome">
+<node CREATED="1332232528672" ID="Freemind_Link_1906367437" MODIFIED="1333271956998" TEXT="Latest (17)">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node COLOR="#ff0000" CREATED="1332232161676" ID="Freemind_Link_261061103" MODIFIED="1335176541230" TEXT="Safari (blocked)">
+<node CREATED="1332232272721" ID="Freemind_Link_1425247602" MODIFIED="1332232277174" TEXT="5.1.2"/>
+<node CREATED="1332232277509" ID="Freemind_Link_1505721707" MODIFIED="1332232392290" TEXT="Latest (5.1.4)"/>
+</node>
+</node>
+</node>
+</map>
